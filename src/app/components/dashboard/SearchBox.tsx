204fba1b05fd53bceee04071a3b3c12f046c8839
adesanyaolu <sanya4reel@gmail.com>
2024-06-29 22:44:28 +0100

    Updated the invited section and the search input field

---
+++ src/app/components/dashboard/SearchBox.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+export default function SearchBox() {
+    return (
+        <div>
+             {/* Search Field */}
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="pl-10 pr-4 py-2 border-none rounded-lg w-60 focus:outline-none focus:border-blue-500 text-tgrey3 bg-tgrey4"
+            aria-label="Search"
+          />
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <Image src="/search.svg" width={15} height={15} alt="Search icon" />
+          </span>
+        </div>
+        </div>
+    )
+
+}

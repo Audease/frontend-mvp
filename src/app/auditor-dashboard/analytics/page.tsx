0a74130c93e9a38531e4416db488dd3ccd8273c9
adesanyaolu <sanya4reel@gmail.com>
2024-08-28 23:58:42 +0100

    Graphs completed for the auditor analytics screen

--- src/app/auditor-dashboard/analytics/page.tsx
+++ src/app/auditor-dashboard/analytics/page.tsx
@@ -1,20 +1,42 @@
 import AuditorDashboardHead from "../auditorDashboardHead";
-import  DoughnutChart , { LineGraph } from "./graphs"
+import DoughnutChart, { FundingGraphs, LineGraph } from "./graphs";
+import { RiDraggable } from "react-icons/ri";
 
 export default function AuditorAnalytics() {
   return (
-    <div className=" overflow-y-auto">
-      {/* BKSD Title and Filter Button */}
+    <div className=" ">
+      {/* Auditor Dahsboard */}
       <AuditorDashboardHead />
 
-      {/* The table  */}
-      <div className="flex flex-col mt-12">
-        <div className="flex flex-row space-x-6 items-center align-center justify-center ">
-            <div className="w-[60%] "><LineGraph /> </div>
-            <div className="w-[35%]"><DoughnutChart /></div>
-        </div>
-        <div className="mt-5">
+      {/* The Graphs  */}
+      <div className="flex flex-col my-12">
+        <div className="flex flex-row space-x-6 justify-center ">
+          <div className="w-[65%]">
             <LineGraph />
+          </div>
+          <div className="w-[30%]">
+            <DoughnutChart />
+          </div>
+        </div>
+
+        {/* The graphs */}
+        <div className="m-6 border border-gray-200 shadow-xl rounded-md p-4 ">
+          <div className="flex flex-row justify-between px-6 items-center">
+            <div>
+              <h3 className="font-medium text-lg">
+              Funding bodies
+              </h3>
+              <p className="font-normal text-sm text-tgrey3">
+              Bodies with graph analysis
+              </p>
+            </div>
+            <div>
+              <RiDraggable className="w-6 h-6" />
+            </div>
+          </div>
+          <div>
+           <FundingGraphs />
+          </div>
         </div>
       </div>
     </div>

15779c5c30a7f23dd545158e9f3765167186ef74
adesanyaolu <sanya4reel@gmail.com>
2025-03-13 13:29:39 +0100

    Changing the file icon

--- src/app/admin/resources/components/FilesInFolder.tsx
+++ src/app/admin/resources/components/FilesInFolder.tsx
@@ -2,17 +2,33 @@ import React, { useEffect, useState } from "react";
 import UploadFileBtn from "./UploadFileBtn";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { MdOutlineCheckBoxOutlineBlank, MdOutlineDeleteForever } from "react-icons/md";
+import {
+  MdOutlineCheckBoxOutlineBlank,
+  MdOutlineDeleteForever,
+} from "react-icons/md";
 import LearnersListModal from "./LearnersListModal";
 import CloudinaryUploader from "./UploadFile";
-import { assignDocToLearners, deleteDocument, postURLToDb } from "../utils/action";
+import {
+  assignDocToLearners,
+  deleteDocument,
+  postURLToDb,
+} from "../utils/action";
 import { DeleteFile } from "./DeleteModal";
+import { CiFolderOn } from "react-icons/ci";
+import { FaFileDownload } from "react-icons/fa";
 
 /**
  * Component to manage files within a folder.
  * Handles file uploads, deletion, and assignment to learners.
  */
-const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchData }) => {
+const FilesInFolder = ({
+  folderId,
+  files,
+  error,
+  loading,
+  createFolder,
+  refetchData,
+}) => {
   const [openLearnersModal, setOpenLearnersModal] = useState(false);
   const [selectedFileId, setSelectedFileId] = useState("");
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
@@ -22,7 +38,7 @@ const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchD
   const [learnersSelected, setLearnersSelected] = useState(false);
   const [actionResponse, setActionResponse] = useState("");
   const [fileId, setFileId] = useState("");
-  const [deleting, setDeleting] =useState(false)
+  const [deleting, setDeleting] = useState(false);
 
   /**
    * Opens modal for assigning a file to learners.
@@ -43,7 +59,11 @@ const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchD
         publicUrl: result.url,
       };
       const status = await postURLToDb(folderId, payload);
-      alert(status === 201 ? "File upload successful" : "An error occurred, try again");
+      alert(
+        status === 201
+          ? "File upload successful"
+          : "An error occurred, try again"
+      );
       refetchData();
     } else {
       alert(result);
@@ -91,8 +111,13 @@ const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchD
    * Handles document assignment to learners.
    */
   const handleAddDocumentClick = async () => {
-    const response = await assignDocToLearners({ documentId: selectedFileId, studentIds: selectedIds });
-    setActionResponse(response === 201 ? "201" : "An error occurred, try again");
+    const response = await assignDocToLearners({
+      documentId: selectedFileId,
+      studentIds: selectedIds,
+    });
+    setActionResponse(
+      response === 201 ? "201" : "An error occurred, try again"
+    );
     setTimeout(() => setActionResponse(""), 10000);
   };
 
@@ -107,10 +132,10 @@ const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchD
   /**
    * Handles file deletion (currently logs ID for implementation later).
    */
-  const handleFileDelete = async() => {
-    setDeleting(true)
-    await deleteDocument(fileId)
-    setDeleting(false)
+  const handleFileDelete = async () => {
+    setDeleting(true);
+    await deleteDocument(fileId);
+    setDeleting(false);
     refetchData();
   };
 
@@ -127,18 +152,39 @@ const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchD
       ) : (
         <ul>
           {files.map((file) => (
-            <div className="ml-10 flex-col flex md:flex-row space-y-2 justify-between my-3" key={file.id}>
-              <div className="flex flex-row justify-start space-x-6">
-                <Image src="/file_icon.png" alt="folder icon" height={10} width={30} />
-                <p className="text-sm text-gray-700 cursor-pointer" onClick={() => handleFileClick(file.publicUrl)}>
+            <div
+              className="ml-10 flex-col flex md:flex-row space-y-2 justify-between my-3"
+              key={file.id}
+            >
+              <div className="flex flex-row justify-start space-x-3">
+                {/* <CiFolderOn /> */}
+                <FaFileDownload className="text-dashboardButtons/100 w-4 h-4"/>
+                {/* <Image
+                  src="/file_icon.png"
+                  alt="folder icon"
+                  height={10}
+                  width={30}
+                /> */}
+                <p
+                  className="text-sm text-gray-700 cursor-pointer"
+                  onClick={() => handleFileClick(file.publicUrl)}
+                >
                   {file.fileName}
                 </p>
               </div>
               <div className="flex flex-row space-x-4 items-center mr-8">
-                <MdOutlineDeleteForever className="text-red-600" onClick={() => openFileDeleteModal(file.id)} />
-                <div className="group flex flex-row items-center space-x-2" onClick={() => assignFiles(file.id)}>
+                <MdOutlineDeleteForever
+                  className="text-red-600"
+                  onClick={() => openFileDeleteModal(file.id)}
+                />
+                <div
+                  className="group flex flex-row items-center space-x-2"
+                  onClick={() => assignFiles(file.id)}
+                >
                   <MdOutlineCheckBoxOutlineBlank className="text-gray-700 group-hover:text-dashboardButtons" />
-                  <p className="text-sm text-gray-700 group-hover:text-dashboardButtons">Assign document</p>
+                  <p className="text-sm text-gray-700 group-hover:text-dashboardButtons">
+                    Assign document
+                  </p>
                 </div>
               </div>
             </div>
@@ -161,12 +207,22 @@ const FilesInFolder = ({ folderId, files, error, loading, createFolder, refetchD
         </div>
       )}
       <div className="flex flex-col justify-center items-center lg:flex-row gap-3">
-        <Button className="bg-dashboardButtons" onClick={() => createFolder(folderId)}>
+        <Button
+          className="bg-dashboardButtons"
+          onClick={() => createFolder(folderId)}
+        >
           Create Sub-Folder
         </Button>
-        <UploadFileBtn onUploadClick={() => setIsUploadModalOpen(!isUploadModalOpen)} />
+        <UploadFileBtn
+          onUploadClick={() => setIsUploadModalOpen(!isUploadModalOpen)}
+        />
       </div>
-      <DeleteFile openModal={openDeleteModal} onClose={() => setOpenDeleteModal(false)} onFileDeleteClick={handleFileDelete} loading={deleting} />
+      <DeleteFile
+        openModal={openDeleteModal}
+        onClose={() => setOpenDeleteModal(false)}
+        onFileDeleteClick={handleFileDelete}
+        loading={deleting}
+      />
     </div>
   );
 };

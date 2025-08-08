3b3a8bf0104bdd9edee14d87e1c1f241f43d5091
adesanyaolu <sanya4reel@gmail.com>
2025-08-08 01:49:38 +0100

    Remove automatic redirection and update Home component to include Login and Sign up buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 "use client";
-
-import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push("https://www.audease.co.uk/")
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className="bg-bgDefault font-switzer h-screen w-full flex flex-row justify-center items-center m-auto space-x-10">
-      <div>Redirecting...</div>
+      <Button className="bg-dashboardButtons" onClick={() => router.push("/signIn")}>Login</Button>
+      <Button className="bg-dashboardButtons" onClick={() => router.push("/signup")}>Sign up</Button>
     </div>
   );
 }

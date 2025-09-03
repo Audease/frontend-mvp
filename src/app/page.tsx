4b8b6b912aab0b66d53482c75292275749cc2202
adesanyaolu <adenugaadesanyao@gmail.com>
2025-09-03 13:53:08 +0100

    Remove login and signup buttons; add automatic redirect to external site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
+
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const router = useRouter();
 
+  useEffect(() => {
+    router.push("https://www.audease.co.uk/")
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="bg-bgDefault font-switzer h-screen w-full flex flex-row justify-center items-center m-auto space-x-10">
-      <Button className="bg-dashboardButtons" onClick={() => router.push("/signIn")}>Login</Button>
-      <Button className="bg-dashboardButtons" onClick={() => router.push("/signup")}>Sign up</Button>
+      <div>Redirecting...</div>
     </div>
   );
 }
